2ecfe1b2ab11e135d5a46ca3f5e3ac9182d2641f
Toril <hokseggentoril@hotmail.com>
2024-05-08 22:56:48 +0200

    managed to make the click to add button work again

--- script.js
+++ script.js
@@ -63,72 +63,72 @@ document.querySelectorAll(".introduction, .important, .ecosystem, .habitat-loss,
 
 //TORIL
 
-// Action button
-// window.addEventListener("scroll", function() {
-//   const scrollButton = document.querySelector(".frog-habitat__action-button");
-//   if (window.scrollY > 9500 && window.scrollY < 9800) {
-//     scrollButton.classList.add("show");
-//   } else {
-//     scrollButton.classList.remove("show");
-//   }
-// });
+//Action button
+window.addEventListener("scroll", function() {
+  const scrollButton = document.querySelector(".frog-habitat__action-button");
+  if (window.scrollY > 9500 && window.scrollY < 9800) {
+    scrollButton.classList.add("show");
+  } else {
+    scrollButton.classList.remove("show");
+  }
+});
 
 // // Function to add image elements to the grass box
-// function addImagesToGrass() {
-//   const image1Coordinates = { x: 800, y: 250 };
-//   const image2Coordinates = { x: 1000, y: 5 };
-//   const image3Coordinates = { x: 300, y: -300 };
+function addImagesToGrass() {
+  const image1Coordinates = { x: 800, y: 250 };
+  const image2Coordinates = { x: 1000, y: 5 };
+  const image3Coordinates = { x: 300, y: -300 };
 
-//   const grassContainer = document.querySelector(".frog-habitat");
+  const grassContainer = document.querySelector(".frog-habitat");
 
-//   let image1 = document.createElement("img");
-//   let image2 = document.createElement("img");
-//   let image3 = document.createElement("img");
+  let image1 = document.createElement("img");
+  let image2 = document.createElement("img");
+  let image3 = document.createElement("img");
 
-//   image1.src = "assets/SVG/leafs.svg";
-//   image2.src = "assets/SVG/pond.svg";
-//   image3.src = "assets/SVG/tree.svg";
+  image1.src = "assets/SVG/leafs.svg";
+  image2.src = "assets/SVG/pond.svg";
+  image3.src = "assets/SVG/tree.svg";
 
-//   image1.height = 100;
-//   image2.height = 300;
-//   image3.height = 500;
+  image1.height = 100;
+  image2.height = 300;
+  image3.height = 500;
 
-//   image1.style.position = "absolute";
-//   image1.style.left = `${image1Coordinates.x}px`;
-//   image1.style.top = `${image1Coordinates.y}px`;
+  image1.style.position = "absolute";
+  image1.style.left = `${image1Coordinates.x}px`;
+  image1.style.top = `${image1Coordinates.y}px`;
 
-//   image2.style.position = "absolute";
-//   image2.style.left = `${image2Coordinates.x}px`;
-//   image2.style.top = `${image2Coordinates.y}px`;
+  image2.style.position = "absolute";
+  image2.style.left = `${image2Coordinates.x}px`;
+  image2.style.top = `${image2Coordinates.y}px`;
 
-//   image3.style.position = "absolute";
-//   image3.style.left = `${image3Coordinates.x}px`;
-//   image3.style.top = `${image3Coordinates.y}px`;
+  image3.style.position = "absolute";
+  image3.style.left = `${image3Coordinates.x}px`;
+  image3.style.top = `${image3Coordinates.y}px`;
 
-//   grassContainer.appendChild(image1);
-//   grassContainer.appendChild(image2);
-//   grassContainer.appendChild(image3);
+  grassContainer.appendChild(image1);
+  grassContainer.appendChild(image2);
+  grassContainer.appendChild(image3);
 
-//   const removeImagesFromGrass = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//       if (!entry.isIntersecting) {
-//         entry.target.remove();
-//       }
-//     });
-//   }, {});
+  const removeImagesFromGrass = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        entry.target.remove();
+      }
+    });
+  }, {});
 
-//   removeImagesFromGrass.observe(image1);
-//   removeImagesFromGrass.observe(image2);
-//   removeImagesFromGrass.observe(image3);
-// }
+  removeImagesFromGrass.observe(image1);
+  removeImagesFromGrass.observe(image2);
+  removeImagesFromGrass.observe(image3);
+}
 
 // // Function to check if an element is in the viewport
-// function isInViewport(element) {
-//   const rect = element.getBoundingClientRect();
-//   return (
-//     rect.top >= 0 &&
-//     rect.left >= 0 &&
-//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//   );
-// }
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
